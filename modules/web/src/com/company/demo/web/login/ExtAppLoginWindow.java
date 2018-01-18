15b41d2b0b74f02dfcba2e06e962a2f7d3eaa326
Yuriy Artamonov <artamonov@haulmont.com>
2018-01-18 23:18:59 +0400

    Migrate to 6.8

--- modules/web/src/com/company/demo/web/login/ExtAppLoginWindow.java
+++ modules/web/src/com/company/demo/web/login/ExtAppLoginWindow.java
@@ -11,8 +11,8 @@ import com.haulmont.cuba.security.entity.User;
 import com.haulmont.cuba.web.App;
 import com.haulmont.cuba.web.Connection;
 import com.haulmont.cuba.web.app.loginwindow.AppLoginWindow;
-import com.haulmont.cuba.web.auth.ExternallyAuthenticatedConnection;
 import com.haulmont.cuba.web.controllers.ControllerUtils;
+import com.haulmont.cuba.web.security.ExternalUserCredentials;
 import com.vaadin.server.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -79,8 +79,7 @@ public class ExtAppLoginWindow extends AppLoginWindow {
                     Connection connection = app.getConnection();
 
                     Locale defaultLocale = messages.getTools().getDefaultLocale();
-                    ((ExternallyAuthenticatedConnection) connection)
-                            .loginAfterExternalAuthentication(user.getLogin(), defaultLocale);
+                    connection.login(new ExternalUserCredentials(user.getLogin(), defaultLocale));
                 } catch (Exception e) {
                     log.error("Unable to login using Facebook", e);
                 } finally {
